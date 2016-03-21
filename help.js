205655b4855793e8365f295f7ddfd2bfbccdc4e2
bhmovva <samvisky1993@gmail.com>
2016-03-20 21:45:34 -0500

    Create help.js

---
+++ help.js
@@ -0,0 +1,9 @@
+function clickedonSubmit() {
+
+  var question = document.getElementById('Question');
+  var email = document.getElementById('Email Address');
+  
+  window.alert("Reply to the question will be sent to the email address " + email.value);
+  
+  
+}
