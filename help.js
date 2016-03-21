a90b008f3aa8f7c72a869987e7a1e992edb1d56b
bhmovva <samvisky1993@gmail.com>
2016-03-20 22:13:03 -0500

    Delete help.js

--- help.js
+++
@@ -1,9 +0,0 @@
-function clickedonSubmit() {
-
-  var question = document.getElementById('Question');
-  var email = document.getElementById('Email Address');
-  
-  window.alert("Reply to the question will be sent to the email address " + email.value);
-  
-  
-}
